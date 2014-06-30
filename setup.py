6c40852848f4df794007f350e7a3877e9cc269fc
chbrun <christophe.brun.cl194@gadz.org>
2014-06-30 23:24:48 +0200

    setup and change directory structure

--- setup.py
+++ setup.py
@@ -10,6 +10,18 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name    = 'testlinkconsole',
     version = '0.1',
+    author = "Christophe Brun",
+    author_email = "christophe.brun.cl194@gadz.org",
+    description = ("Testlink Console "),
+    license = "BSD",
+    keywords = "console testlink",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=find_packages(),
+    classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: BSD License",
+            ],
 
 
 )
