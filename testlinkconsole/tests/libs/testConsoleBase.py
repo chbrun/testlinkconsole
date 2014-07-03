b773326cde062592d08fd20dd8fabc52fb060f59
chbrun <christophe.brun.cl194@gadz.org>
2014-07-04 00:19:31 +0200

    add tests

--- testlinkconsole/tests/libs/testConsoleBase.py
+++ testlinkconsole/tests/libs/testConsoleBase.py
@@ -12,9 +12,16 @@ class TestconsoleBase(unittest.TestCase):
 
     def test_help_config(self):
         self.assertEquals(self.consoleBase.help_config(),None)
-        
+    
+    @unittest.skip('todo')
     def test_help_save(self):
         self.assertEquals(self.consoleBase.help_save(),None)
         
+    def test_help_get(self):
+        self.assertEquals(self.consoleBase.help_get(),None)
+    
+    def test_help_set(self):
+        self.assertEquals(self.consoleBase.help_set(),None)
+
 if __name__ == '__main__':
     unittest.main()
